79ea9bd0ee35439eff5148d6d39197905104db9d
Daniel Rasmussen <daniel.rasmussen@appliedbrainresearch.com>
2019-03-14 16:50:24 -0300

    WIP template test/coverage/docs/examples

--- .ci/bones_conf.py
+++ .ci/bones_conf.py
@@ -1,4 +1,20 @@
+pkg_name = "nengo_dl"
+repo_name = "nengo/nengo-dl"
+
+# ci script config
 script_config = {
-    "static": {"pip_install": ["-e .[docs]"],
-               "conda_install": ["pandoc"]}
+    "static": {
+        "pip_install": ["-e .[docs]"],
+        "conda_install": ["pandoc"]},
+    "test": {
+        "pip_install": [
+            "-e .[tests] $NUMPY_VERSION $TF_VERSION $NENGO_VERSION"],
+        "nengo_tests": True},
+    "examples": {
+        "pip_install": ["-e .[docs]"],
+        "custom_commands": ["python docs/whitepaper/whitepaper2018_code.py > /dev/null"]},
+    "docs": {
+        "pip_install": ["-e .[docs]"],
+        "conda_install": ["pandoc"]
+    }
 }
