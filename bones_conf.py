1982a690fc49fd6a6e6e59e62d8e6be104004936
Daniel Rasmussen <daniel.rasmussen@appliedbrainresearch.com>
2019-03-15 12:52:56 -0300

    fixup scripts

--- bones_conf.py
+++ bones_conf.py
@@ -4,14 +4,14 @@ repo_name = "nengo/nengo-dl"
 # ci script config
 script_config = {
     "static": {
-        "pip_install": ["-e .[docs]"],
+        "pip_install": ["-e .[all]"],
         "conda_install": ["pandoc"]},
     "test": {
         "pip_install": [
             "-e .[tests] $NUMPY_VERSION $TF_VERSION $NENGO_VERSION"],
         "nengo_tests": True},
     "examples": {
-        "pip_install": ["-e .[docs]"],
+        "pip_install": ["-e .[all]"],
         "custom_commands": ["python docs/whitepaper/whitepaper2018_code.py > /dev/null"]},
     "docs": {
         "pip_install": ["-e .[docs]"],
